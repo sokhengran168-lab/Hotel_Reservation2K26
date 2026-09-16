ac5344748697810adb51ba17cf8a7680a1fb7c90
sokhengran168-lab <sokhengran168@example.com>
2026-09-16 12:02:25 +0700

    Bypass broken cloudinary-labs wrapper, use raw Cloudinary SDK directly

---
+++ config/cloudinary.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+    'cloud_url' => env('CLOUDINARY_URL'),
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+    'upload_route' => env('CLOUDINARY_UPLOAD_ROUTE'),
+    'upload_action' => env('CLOUDINARY_UPLOAD_ACTION'),
+];
